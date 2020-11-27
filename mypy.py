b12066ad9f81704f4d8c1210c8393e36fff88d45
Adamthe1st <aada071@aucklanduni.ac.nz>
2020-11-27 10:22:55 +0300

    Update mypy.py

--- mypy.py
+++ mypy.py
@@ -12,3 +12,14 @@ def positive_sum(arr):  #this is the definition satatement of the function being
         if x > 0:
             total = total + num
     return total
+
+#Create a function (or write a script in Shell) 
+#that takes an integer as an argument 
+#and returns "Even" for even numbers or "Odd" for odd numbers.
+#Answer:
+def even_or_odd(number): #this is our definition statement for the function
+    if number % 2 == 0:   # the '%' is modulu that returns the reminder of a devision.
+        #here, any number devided by 2 and has 0 reminder must be an even number (just remember this even if it doesnt make sense)
+        return 'Even'
+    else:
+        return 'Odd'
